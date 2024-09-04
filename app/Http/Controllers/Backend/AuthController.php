db3f05f0597c303eb32c601c016c250cbd414610
Carlos Eduardo Rivera Franco <carlos_2017_1@hotmail.com>
2024-09-03 21:40:53 -0500

    Obteniendo token de inicio de sesión

---
+++ app/Http/Controllers/Backend/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'msg' => 'incorrect username or password'
+            ], 401);
+        }
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        $res = [
+            'token' => $token,
+            'user' => $user,
+        ];
+
+        return response($res, 201);
+    }
+}
